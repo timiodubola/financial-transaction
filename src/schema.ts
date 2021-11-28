f4cccd5b9881e69a943577190bd5776e7051a1ba
timiodubola <odubolaoluwatimilehin@gmail.com>
2021-11-28 19:56:51 +0100

    feat: Apollo server wired up, get all endpoint added

---
+++ src/schema.ts
@@ -0,0 +1,37 @@
+import { gql } from 'apollo-server'
+import { DateTimeResolver } from 'graphql-scalars'
+import { Context } from './context'
+
+export const typeDefs = gql`
+  type Transaction {
+    id:              String!  
+    account:         String
+    description:     String
+    category:        String
+    reference:       String
+    currency:        String
+    amount:          Int
+    status:          String
+    transactionDate: DateTime
+    createdAt:       DateTime
+    updatedAt:       DateTime
+  }
+
+  type Query {
+    allTransaction: [Transaction!]!
+  }
+
+
+ scalar DateTime
+
+`
+
+export const resolvers = {
+  Query: {
+    allTransaction : (_parent: unknown, _args: unknown , context: Context) => {
+      return context.prisma.transaction_.findMany()
+    }   
+  },
+  DateTime: DateTimeResolver
+}
+
---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import { ApolloServer } from 'apollo-server'
+import { resolvers, typeDefs } from './schema'
+import { context } from './context'
+
+new ApolloServer({ resolvers, typeDefs, context: context }).listen(
+  { port: 4000 },
+  () =>
+    console.log(` Server ready at: http://localhost:4000`),
+)

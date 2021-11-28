10a51fe8f522daf2bb969e2843caec4be63e2759
timiodubola <odubolaoluwatimilehin@gmail.com>
2021-11-28 20:08:33 +0100

    feat: server port fixed

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import { resolvers, typeDefs } from './schema'
 import { context } from './context'
 
 new ApolloServer({ resolvers, typeDefs, context: context }).listen(
-  { port: 4000 },
+  { port: process.env.PORT || 4000 },
   () =>
     console.log(` Server ready at: http://localhost:4000`),
 )
